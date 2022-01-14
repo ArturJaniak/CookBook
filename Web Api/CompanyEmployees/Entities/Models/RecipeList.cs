49fbe977976de01504028cbe519488a53db9d94e
xmen2237 <45273004+xmen2237@users.noreply.github.com>
2022-01-14 17:00:40 +0100

    migracje5

--- Web Api/CompanyEmployees/Entities/Models/RecipeList.cs
+++ Web Api/CompanyEmployees/Entities/Models/RecipeList.cs
@@ -13,8 +13,8 @@ namespace Entities.Models
         public Guid RecipeId { get; set; }
         public Recipes Recipes { get; set; }
 
-        [ForeignKey(nameof(User))]
-        public Guid UserId { get; set; }
-        public User User { get; set; }
+        //[ForeignKey(nameof(User))]
+        //public Guid UserId { get; set; }
+        //public User User { get; set; }
     }
 }
