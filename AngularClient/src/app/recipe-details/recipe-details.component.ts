0324d33a4817208645531104ee8060744e1c49ae
Artur Janiak <Artur.t.janiak@gmail.com>
2022-05-04 16:35:45 +0200

    Poprawne wyświetlanie obrazka, aczkolwiek dopiero po kompilacji #3

--- AngularClient/src/app/recipe-details/recipe-details.component.ts
+++ AngularClient/src/app/recipe-details/recipe-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { RecipesClient, RecipesLogedClient } from '../api/ApiClient';
 import { SharingService } from '../shared/sharing.service';
 
@@ -12,46 +12,42 @@ import { SharingService } from '../shared/sharing.service';
 export class RecipeDetailsComponent implements OnInit {
 
   constructor(private recipesClient: RecipesClient,
-     private sharingService: SharingService,
-     private recipesLogged: RecipesLogedClient,
-     private router: Router,
-     ){}
-  recipe$ : Observable<any>;
-  id:any;
-  recipe:any=[];
+    private sharingService: SharingService,
+    private recipesLogged: RecipesLogedClient,
+    private router: Router,
+  ) { }
+  recipe$: Observable<any>;
+  id: any;
+  recipe: any = [];
 
 
 
 
-  rateValue:number;
-  rating : number;
-  rate:any
-  newRecipe:any=[];
+  rateValue: number;
+  rating: number;
+  rate: any
+  newRecipe: any = [];
   ngOnInit(): void {
-    this.id =  this.sharingService.getData();
+    this.id = this.sharingService.getData();
     //this.route.params.subscribe(params => {
     this.getThatRecipe();
     //})
-    
-    }
-  rateRecipe(rateValue){
-    //this.id = this.id;
-      this.rateValue = rateValue;
-     // console.log("recipe "+ this.recipe + " "+ this.recipe.rating);
-      this.recipesLogged.recipesLoged_RateRecipe(localStorage.getItem("token"), this.id, rateValue).subscribe(res=>(this.recipe.rating) = res);
-      //console.log("recipe rating" + this.recipe.rating +" "+ this.id);
-      //this.ngOnInit();
-      //this.getThatRecipe();
-    
+
   }
-  getThatRecipe(){
-    this.recipesClient.recipes_GetRecipe(this.id).subscribe(res=>(this.recipe = res));
+  rateRecipe(rateValue) {
+    this.rateValue = rateValue;
+
+    this.recipesLogged.recipesLoged_RateRecipe(localStorage.getItem("token"), this.id, rateValue).subscribe(res => (this.recipe.rating) = res);
+
   }
-  editRecipeDetail(recipe_id : any){
+  getThatRecipe() {
+    this.recipesClient.recipes_GetRecipe(this.id, localStorage.getItem("token")).subscribe(res => (this.recipe = res));
+  }
+  editRecipeDetail(recipe_id: any) {
     let url: string = "/editDetailsRecipe/" + recipe_id
-         this.router.navigateByUrl(url);
-         //console.log(recipe_id);
-         this.sharingService.setData(recipe_id);
-      }
-  
+    this.router.navigateByUrl(url);
+    //console.log(recipe_id);
+    this.sharingService.setData(recipe_id);
+  }
+
 }
